530da1f1aca038b941407fefb0fd3ea9efdbb73a
swjo <jjo6734@gmail.com>
2018-10-02 15:46:39 +0800

    Fix move_bundle.sh

--- move_bundle.sh
+++ move_bundle.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "move to Web-Frontend Repo"
+FrontendRepository="../additor-web"
 
-cp -f dist/quill.js ../additor-web/node_modules/@additor/quill/dist/quill.js
-cp -f dist/quill.core.js ../additor-web/node_modules/@additor/quill/dist/quill.core.js
-cp -f dist/unit.js ../additor-web/node_modules/@additor/quill/dist/unit.js
+cp -f dist/quill.js $FrontendRepository/node_modules/@additor/quill/dist/quill.js
+cp -f dist/quill.core.js $FrontendRepository/node_modules/@additor/quill/dist/quill.core.js
+cp -f dist/unit.js $FrontendRepository/node_modules/@additor/quill/dist/unit.js
